d1fdbbef717e0d51ce59eca28ad1f2cc28e31572
Adam Uhlir <adam@uhlir.dev>
2022-02-07 20:16:20 +0100

    build: brew formula update for loggy@v0.3.0

--- loggy.rb
+++ loggy.rb
@@ -5,21 +5,21 @@
 class Loggy < Formula
   desc "Swiss knife for logs."
   homepage "https://github.com/auhau/loggy"
-  version "0.2.0"
+  version "0.3.0"
   depends_on :macos
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/AuHau/loggy/releases/download/v0.2.0/loggy-darwin-arm64.tar.gz"
-      sha256 "cf22436c02f15c0517dd4a574f0502473418db8984fca6e4f2c42cf2885117a9"
+      url "https://github.com/AuHau/loggy/releases/download/v0.3.0/loggy-darwin-arm64.tar.gz"
+      sha256 "142eecd55bd2602848d1cff32640ad766ad858d7ab8f18943e583db05b997c10"
 
       def install
         bin.install "loggy"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/AuHau/loggy/releases/download/v0.2.0/loggy-darwin-amd64.tar.gz"
-      sha256 "5c1221d69821ac7a0a36b0aac15cecd2eeec1ae0cbe545c32a0325f029d04f5e"
+      url "https://github.com/AuHau/loggy/releases/download/v0.3.0/loggy-darwin-amd64.tar.gz"
+      sha256 "e7d2ec27caf6157b23fbb63da4cb613f58a81656fddbf1df7100a7ea46c620a3"
 
       def install
         bin.install "loggy"
